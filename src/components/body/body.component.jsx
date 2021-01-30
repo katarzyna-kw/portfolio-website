f1638029cd07ee067c2b74308d90a2eb763068c0
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-29 19:44:21 -0600

    Initial commit with site structure

---
+++ src/components/body/body.component.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function Body() {
+  return (
+    <div class="body">
+    <p>this is the body</p>
+  </div>
+  
+  );
+}
+
+export default Body;
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function Footer() {
+  return (
+    <div class="footer">
+      <p>This is the footer</p>
+    </div>
+  
+  );
+}
+
+export default Footer;
