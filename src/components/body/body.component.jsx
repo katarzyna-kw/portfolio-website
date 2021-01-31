0c67ff793402031cf1dd8e9b6c38ea7ca8b8ed9c
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-31 16:52:15 -0600

    Added email envelope icon

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -6,6 +6,8 @@ function Body() {
      <div id="about">
        <h2>About</h2>
         <img class="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle.png?raw=true" alt="image of Katarzyna" />
+        <img class="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle-bw.png?raw=true" alt="image of Katarzyna" />
+
         My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with expertise in communication skills and background in education. My skills include Javascript React, Javascript, HTML, and CSS. I love colors, animals, and Dionne Warwick.
     </div>
     <div id="portfolio">
@@ -13,9 +15,18 @@ function Body() {
     </div>
     <div id="contact">
       <h2>Contact</h2>
-      
+      <ul>
+        <li>
+          <img src="" alt="email envelope icon" />
+        </li>
+        <li>
+          <img src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/icon-github.png?raw=true" alt="github icon" />
+        </li>
+        <li>
+          <img src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/icon-linkedin.png?raw=true" alt="linkedin icon" />
+        </li>
+      </ul>
     </div>
-  </div>
   );
 }
 
