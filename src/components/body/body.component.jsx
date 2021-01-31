ec17d33d92ef7762b1ddecd4591a9ba3926310e9
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-31 16:46:44 -0600

    Added contact png icons

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -3,9 +3,17 @@ import './styles.css'
 function Body() {
   return (
     <div class="body">
-    <div id="about">
-      <img class="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot.png?raw=true" alt="image of Katarzyna" />
-      I am a front-end developer based in Chicago with expertise in communication skills and background in education. My skills include Javascript React, Javascript, HTML, and CSS. I love colors, animals, and Dionne Warwick.
+     <div id="about">
+       <h2>About</h2>
+        <img class="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle.png?raw=true" alt="image of Katarzyna" />
+        My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with expertise in communication skills and background in education. My skills include Javascript React, Javascript, HTML, and CSS. I love colors, animals, and Dionne Warwick.
+    </div>
+    <div id="portfolio">
+
+    </div>
+    <div id="contact">
+      <h2>Contact</h2>
+      
     </div>
   </div>
   );
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,8 @@ import './styles.css'
 function Footer() {
   return (
     <div class="footer">
-      <p>© 2021</p>
+      <p>© 2021 Katarzyna Wegrzynowicz //
+      Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
     </div>
   
   );
