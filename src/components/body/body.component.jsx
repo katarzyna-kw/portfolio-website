dd95218e4aa3c2e74881f9e2758b7bdcbebfe1ba
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-31 16:25:55 -0600

    Added png

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -4,6 +4,7 @@ function Body() {
   return (
     <div class="body">
     <div id="about">
+      <img class="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot.png?raw=true" alt="image of Katarzyna" />
       I am a front-end developer based in Chicago with expertise in communication skills and background in education. My skills include Javascript React, Javascript, HTML, and CSS. I love colors, animals, and Dionne Warwick.
     </div>
   </div>
