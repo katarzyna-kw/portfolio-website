77bf5ff5fd33d3b5e5605eda70ed6cc0123d11a6
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-02 07:14:51 -0600

    Initialized media queries for tablet and desktop for footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div class="footer">
       <p>© 2021 Katarzyna Wegrzynowicz //
         Icons made by 
-          <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy" target="_blank">Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a>
+        <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy" target="_blank"> Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a>
       </p>
     </div>
   
