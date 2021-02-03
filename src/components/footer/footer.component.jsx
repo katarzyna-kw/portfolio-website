c891c901fdc8b019addfa94d105e5cc2853c8258
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-03 07:18:14 -0600

    Ran footer component test

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 function Footer() {
   return (
-    <div className="footer" data-test-id="footer">
+    <div className="footer" data-testid="Footer">
       <p>© 2021 Katarzyna Wegrzynowicz //
         Icons made by 
         <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy" target="_blank"> Dave Gandy</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank">www.flaticon.com</a>
--- src/components/footer/footer.component.test.js
+++ src/components/footer/footer.component.test.js
@@ -3,6 +3,6 @@ import Footer from './footer.component';
 
 test('renders learn react link', () => {
   render(<Footer />);
-  const linkElement = screen.getByTestId("footer");
+  const linkElement = screen.getByTestId("Footer");
   expect(linkElement).toBeInTheDocument();
 });
