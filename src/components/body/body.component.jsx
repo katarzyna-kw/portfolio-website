1ef053153f0c2ac8e13ad9db6e3f270f8551d144
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-04 22:17:06 -0600

    Added functions for mouseOver and mouseOut to show about img in color during hover and in bw otherwise

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -6,7 +6,8 @@ function Body() {
      <div id="about">
        <h2>About</h2>
         {/* <img className="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle.png?raw=true" alt="image of Katarzyna" /> */}
-        <img className="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle-bw.png?raw=true" alt="Katarzyna Wegrzynowicz" />
+
+        <img className="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle-bw.png?raw=true" alt="Katarzyna Wegrzynowicz" onMouseOver={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle.png?raw=true")} onMouseOut={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/img/headshot-circle-bw.png?raw=true")}/>
 
         My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with a background in education and expertise in active listening and communication. My coding skills include React JS, Javascript, HTML, and CSS. I love colors, animals, and Dionne Warwick.
       </div>
