ca418ca193f9f194d97a98c5ff2d2cf18e07c420
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-04 22:02:15 -0600

    Begin building pportfolio slideshow with bootstrap

--- src/components/body/body.component.jsx
+++ src/components/body/body.component.jsx
@@ -48,7 +48,30 @@ function Body() {
           </li>
         </ul>
       </div>
+
+
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/portfolio-election-map.png?raw=true" alt="Screencapture of election map project built with Javascript" />
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/portfolio-quote-generator.png?raw=true" alt="Screencapture of responsive quote generator coded with vanilla Javascript" />
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/portfolio-cat-clock.png?raw=true" alt="Screencapture of clock coded with Javascript" />
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
+  </div>
   );
 }
 
