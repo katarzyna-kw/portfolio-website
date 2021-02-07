08eeb370a5905e5abfe451c464d225eda4b270fb
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-06 21:17:11 -0600

    Work in progress with links in slideshow

--- src/components/body/SliderContent.js
+++ src/components/body/SliderContent.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import sliderImage from './sliderImage';
-import { Link } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
+  
 
 function SliderContent(props) {
     return (
+        // <Router>
         <section>
             {sliderImage.map((slide, index) => (
                 <div 
                     key={index}
                     className={index === props.activeIndex ? "slides active" : "inactive"}
                 >
-                    {/* <Link to={`${slide.link}}`}> */}
+                    {/* <Link to={`${slide.link}` */}
+
+            {/* }> */}
                         <img className="slide-image" src={slide.urls} alt={slide.alt} />
                         {/* </Link> */}
                     <h5 className="slide-text">{slide.description}</h5>
                 </div>
-            ))}
+            ))
+}
         </section>
+        // </Router>
     );
 }
 
