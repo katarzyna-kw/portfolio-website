91d3dc4ede0608536ae75a7356e4da3983aeb5e5
katarzyna_w <katarzyna.koch@gmail.com>
2021-05-02 20:16:57 -0500

    update summary of cat clock in Swipe.js

--- src/components/body/Swipe.js
+++ src/components/body/Swipe.js
@@ -44,7 +44,7 @@ export default () => {
                 src="https://github.com/katarzyna-kw/portfolio-website/blob/main/img/2.png?raw=true" 
                 alt="cat clock screencapture"
             />
-            <h5>Clock coded with Javascript and styled given html and template css code // <a href="https://www.skillcrush.com" target="_blank" rel="noreferrer">Skillcrush</a> project // <a href="https://github.com/katarzyna-kw/lolcat" target="_blank" rel="noreferrer">github repo</a></h5>
+            <h5>Clock coded with Javascript and styled with flexbox given html and template css code // <a href="https://www.skillcrush.com" target="_blank" rel="noreferrer">Skillcrush</a> project // <a href="https://github.com/katarzyna-kw/lolcat" target="_blank" rel="noreferrer">github repo</a></h5>
         </SwiperSlide>
         <SwiperSlide>
             <img 
