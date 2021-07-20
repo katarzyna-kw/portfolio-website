e79273431efce7c9adbf23476bd667ef9d8008be
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-20 17:49:33 -0500

    add more projects to data.js

--- src/components/body/SwiperData.js
+++ src/components/body/SwiperData.js
@@ -4,7 +4,7 @@ module.exports = [
       imageid: '01.png',
       name: 'Github Repo Gallery',
       description: 'Displays public repos with search functionality. Javascript coded for Skillcrush challenge/learning purposes. HTML/CSS template customized to match portfolio website and to enable light/dark themes to enhance user experience.',
-      languages: 'Vanilla Javascript, CSS, HTML',
+      languages: 'Javascript, CSS, HTML',
       link: 'github-repo-gallery',
       repo: 'github-repo-gallery'
     },
@@ -13,7 +13,7 @@ module.exports = [
         imageid: '02.png',
         name: 'Puffy Cat Game ',
         description: 'Puffy Cat version of Flappy Bird coded with Javascript and HTML5 canvas and designed with custom graphics.',
-        languages: 'Vanilla Javascript, HTML5',
+        languages: 'Javascript, CSS, HTML5',
         link: 'puffy-cat-game',
         repo: 'puffy-cat-game'
       }, 
@@ -22,7 +22,7 @@ module.exports = [
         imageid: '03.png',
         name: 'Sticky Notes Generator ',
         description: 'To-do app coded using React JS. HTML/CSS starter template customization.',
-        languages: 'React JS',
+        languages: 'React JS, CSS',
         link: 'sticky-notes',
         repo: 'sticky-notes'
     }, 
@@ -31,7 +31,7 @@ module.exports = [
         imageid: '04.png',
         name: 'Daily Mood Check ',
         description: 'A functional mood tracker built with React JS that uses Google Forms as an API.',
-        languages: 'React JS',
+        languages: 'React JS, CSS',
         link: 'daily-check',
         repo: 'daily-check'
     }, 
@@ -40,7 +40,7 @@ module.exports = [
         imageid: '05.png',
         name: 'Youtube Clone ',
         description: 'Created for learning purposes using React JS and Node JS Express/PostgreSQL for server. Work in Progress. Currently renders videos from database and increments likes/dislikes for each video.',
-        languages: 'React JS, Javascript through Node JS',
+        languages: 'React JS, Javascript through Node JS, CSS',
         link: 'youtube-clone',
         repo: 'youtube-clone'
     }, 
@@ -49,8 +49,62 @@ module.exports = [
         imageid: '06.png',
         name: 'Guess the Word Game ',
         description: 'A guessing game in which the player has 8 attempt to guess the correct word. Javascript coded for Skillcrush challenge/learning purposes. HTML/CSS starter template customization.',
-        languages: 'React JS, Javascript through Node JS',
+        languages: 'Javascript, HTML, CSS',
         link: 'guess-the-word-game',
         repo: 'guess-the-word-game'
-    }
-  ]
+    },
+    {
+      id: '07',
+      imageid: '07.png',
+      name: 'Lodash Clone',
+      description: 'An academic exercise writing code to mimic lodash library methods such as first, last, forEach, filter, map, and reduce.',
+      languages: 'Javascript',
+      link: 'lodash-clone',
+      repo: 'lodash-clone'
+    },
+    {
+      id: '08',
+      imageid: '08.png',
+      name: 'Election Map',
+      description: 'An interactive 2020 U.S. presidential election map built using vanilla Javascript for a Skillcrush project. HTML/CSS starter template customization.',
+      languages: 'Javascript, CSS, HTML',
+      link: 'election-map-project',
+      repo: 'election-map-project'
+  },
+  {
+    id: '09',
+    imageid: '09.png',
+    name: 'Cat Clock',
+    description: 'A responsive clock with dark and light modes built with vanilla Javascript for a Skillcrush project. HTML/CSS starter template customization.',
+    languages: 'Javascript, HTML, CSS',
+    link: 'lolcat',
+    repo: 'lolcat'
+  },
+  {
+    id: '10',
+    imageid: '10.png',
+    name: 'Quote Generator',
+    description: 'A responsive quote generator made using vanilla JS, HTML, and CSS for learning purposes.',
+    languages: 'Javascript, HTML, CSS',
+    link: 'quote-generator',
+    repo: 'quote-generator'
+  },
+  {
+    id: '11',
+    imageid: '11.png',
+    name: 'CSS Technical Documentation',
+    description: 'A responsive mock technical documentation page about CSS built for a freecodecamp project using HTML and CSS.',
+    languages: 'HTML, CSS',
+    link: 'fcc-challenge-4',
+    repo: 'fcc-challenge-4'
+  },
+  {
+    id: '12',
+    imageid: '12.png',
+    name: 'Mock School Site',
+    description: 'A responsive mock website for a Chicago-based Polish language school built for a freecodecamp project using HTML and CSS.',
+    languages: 'HTML, CSS',
+    link: 'fcc-challenge-3',
+    repo: 'fcc-challenge-3'
+  },
+];
