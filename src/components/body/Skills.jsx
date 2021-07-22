fc66e3aac83df4c081f000b9f1c7a3a46e10adfd
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-22 13:01:42 -0500

    skills section responsive

--- src/components/body/Skills.jsx
+++ src/components/body/Skills.jsx
@@ -6,26 +6,25 @@ function Skills() {
         <div id="skills">
             <h2>Skills</h2>
             <div className="skills-content">
-                <div className="skills-content-col">
+                <div className="skills-content-col1">
                     <h5>Proficient</h5>
                     <ul>
-                        <li>JavaScript</li>
-                        <li>React JS</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>Git</li>
-                        <li>Photoshop</li>
-                        <li>PostgreSQL</li>
+                        <li><span>JavaScript</span></li>
+                        <li><span>React JS</span></li>
+                        <li><span>HTML</span></li>
+                        <li><span>CSS</span></li>
+                        <li><span>Git</span></li>
+                        <li><span>Photoshop</span></li>
                     </ul>
                 </div>
-                <div className="skills-content-col">
+                <div className="skills-content-col2">
                     <h5>Competent</h5>
                     <ul>
                         <li>PostgreSQL</li>
                         <li>Jasmine testing</li>
                     </ul>
                 </div>
-                <div className="skills-content-col">
+                <div className="skills-content-col3">
                     <h5>Fluent Languages</h5>
                     <ul>
                         <li>English</li>
