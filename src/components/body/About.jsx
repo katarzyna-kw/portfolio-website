7c8501c898fd2fb101d129df93853e77609f9c5c
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-21 22:37:15 -0500

    remove whitespace in about section

--- src/components/body/About.jsx
+++ src/components/body/About.jsx
@@ -5,10 +5,22 @@ function About() {
   return (
         <div id="about">
             <h2>About</h2>
-            <img className="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true" alt="Katarzyna Wegrzynowicz" onMouseOver={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle.png?raw=true")} onMouseOut={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true")}/>
-
-            My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with a background in education and expertise in active listening and communication. My programming skills include Javascript, React JS, CSS, and HTML5. I also have some back-end experience using PostgreSQL. I love colors, animals, solving problems, and Dionne Warwick.
-        </div>
+            <div className="about-content">
+              <img className="img" src="https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true" alt="Katarzyna Wegrzynowicz" onMouseOver={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle.png?raw=true")} onMouseOut={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true")}/>
+              <p>
+                My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with a background in education and expertise in active listening and communication. My programming skills include Javascript, React JS, CSS, and HTML5. I also have some back-end experience using PostgreSQL. I love colors, animals, solving problems, and Dionne Warwick.
+              </p>
+            </div>
+            {/* <div class="about-skills">
+              <h2>skills</h2>
+              <ul>
+                <li>Javascript</li>
+                <li>React JS</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
+            </div> */}
+          </div>
   );
 }
 
