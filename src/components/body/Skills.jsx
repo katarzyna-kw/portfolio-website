e58a8cbe2ecc669772b52f02e2c2ce49593118f4
katarzyna_w <katarzyna.koch@gmail.com>
2021-07-25 21:48:52 -0500

    remove middle col in skills and reformat

--- src/components/body/Skills.jsx
+++ src/components/body/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
             <h2>Skills</h2>
             <div className="skills-content">
                 <div className="skills-content-col1">
-                    <h5>Proficient</h5>
+                    <h5>Technical</h5>
                     <ul>
                         <li><span>JavaScript</span></li>
                         <li><span>React JS</span></li>
@@ -15,15 +15,17 @@ function Skills() {
                         <li><span>CSS</span></li>
                         <li><span>Git</span></li>
                         <li><span>Photoshop</span></li>
+                        <li><span>PostgreSQL</span></li>
+                        <li><span>Jasmine testing</span></li>
                     </ul>
                 </div>
-                <div className="skills-content-col2">
+                {/* <div className="skills-content-col2">
                     <h5>Limited Proficiency</h5>
                     <ul>
                         <li>PostgreSQL</li>
                         <li>Jasmine testing</li>
                     </ul>
-                </div>
+                </div> */}
                 <div className="skills-content-col3">
                     <h5>Fluent Languages</h5>
                     <ul>
