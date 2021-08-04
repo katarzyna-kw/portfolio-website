83a8005b0663498f59b488665ba806d8edd87273
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-04 16:42:26 -0500

    img in about

--- src/components/body/About.jsx
+++ src/components/body/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <div id="about">
             <h2>About</h2>
             <div className="about-content">
-              <img className="img" src={"https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/ocean.png"} alt="Katarzyna Wegrzynowicz" onMouseOver={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle.png?raw=true")} onMouseOut={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true")}/>
+              <img className="img" src={"https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true"} alt="Katarzyna Wegrzynowicz" onMouseOver={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle.png?raw=true")} onMouseOut={e => (e.currentTarget.src = "https://github.com/katarzyna-kw/portfolio-website/blob/main/public/img/headshot-circle-bw.png?raw=true")}/>
               <p>
                 My name is Katarzyna Wegrzynowicz. I am a front-end developer based in Chicago with a background in education and expertise in active listening and communication. My programming skills include Javascript, React JS, CSS, and HTML. I also have some back-end experience using PostgreSQL. I love colors, animals, solving problems, and Dionne Warwick.
               </p>
