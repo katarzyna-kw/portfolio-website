5481b6f730c237c54668950774b35170f85f9647
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-05 09:25:41 -0500

    tests

--- src/components/body/body.component.test.js
+++ src/components/body/body.component.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import Body from './body.component';
 
-test('renders learn react link', () => {
+test('renders Body', () => {
   render(<Body />);
-  const linkElement = screen.getByTestId("Body");
-  expect(linkElement).toBeInTheDocument();
+  const body = screen.getByTestId("Body");
+  expect(body).toBeInTheDocument();
 });
