6059259df736b5942e8dda3a417c102f6a707204
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-06 18:00:27 -0500

    debug swiper

--- src/components/body/Swipe.js
+++ src/components/body/Swipe.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {SwiperCard} from './SwiperCard'
+// import {SwiperCard} from './SwiperCard'
 import { data } from './SwiperData'
 
 // Import Swiper React components
@@ -30,54 +30,55 @@ function Swipe() {
         onSwiper={() => console.log('swipe!')}
         onSlideChange={() => console.log('slide change')}
       >
+
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[0].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[0].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[0].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[0].name} <span>/// Built using {data[0].languages}</span></h3>
                     <p>{data[0].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[0].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[0].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[0].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[1].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[1].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[1].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[1].name} <span>/// Built using {data[1].languages}</span></h3>
                     <p>{data[1].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[1].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[1].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[1].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[2].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[2].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[2].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[2].name} <span>/// Built using {data[2].languages}</span></h3>
                     <p>{data[2].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[2].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[2].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[2].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[3].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[3].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[3].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[3].name} <span>/// Built using {data[3].languages}</span></h3>
                     <p>{data[3].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[3].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[3].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[3].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
@@ -97,13 +98,13 @@ function Swipe() {
         </SwiperSlide> */}
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[5].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[5].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[5].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[5].name} <span>/// Built using {data[5].languages}</span></h3>
                     <p>{data[5].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[5].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[5].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[5].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
@@ -123,65 +124,65 @@ function Swipe() {
         </SwiperSlide> */}
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[7].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[7].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[7].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[7].name} <span>/// Built using {data[7].languages}</span></h3>
                     <p>{data[7].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[7].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[7].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[7].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[8].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[8].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[8].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[8].name} <span>/// Built using {data[8].languages}</span></h3>
                     <p>{data[8].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[8].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[8].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[8].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[9].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[9].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[9].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[9].name} <span>/// Built using {data[9].languages}</span></h3>
                     <p>{data[9].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[9].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[9].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[9].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[10].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[10].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[10].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[10].name} <span>/// Built using {data[10].languages}</span></h3>
                     <p>{data[10].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[10].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[10].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[10].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[11].link}`} target="_blank" rel="noreferrer">
+                <a href={`${data[11].link}`} target="_blank" rel="noreferrer">
                     <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[11].imageid}`} alt='screencapture' />
                 </a>
                 <div className="cardTextWrapper">
                     <h3>{data[11].name} <span>/// Built using {data[11].languages}</span></h3>
                     <p>{data[11].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[11].link}`} target="_blank" rel="noreferrer">Live link</a></div>
+                    <div className="cardLinks"><a href={`${data[11].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                     <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[11].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
                 </div>
             </div>
@@ -194,26 +195,11 @@ function Swipe() {
                 <div className="cardTextWrapper">
                     <h3>{data[12].name} <span>/// Built using {data[12].languages}</span></h3>
                     <p>{data[12].description}</p>
-                    <div className="cardLinks"><a href="https://606basketballacademy.com/" target="_blank" rel="noreferrer">Live link</a></div>
-                </div>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide>
-            <div className="cardWrapper">
-                <a href={`https://katarzyna-kw.github.io/${data[13].link}`} target="_blank" rel="noreferrer">
-                    <img src={`https://raw.githubusercontent.com/katarzyna-kw/portfolio-website/main/public/img/portfolio/${data[13].imageid}`} alt='screencapture' />
-                </a>
-                <div className="cardTextWrapper">
-                    <h3>{data[13].name} <span>/// Built using {data[13].languages}</span></h3>
-                    <p>{data[13].description}</p>
-                    <div className="cardLinks"><a href={`https://katarzyna-kw.github.io/${data[13].link}`} target="_blank" rel="noreferrer">Live link</a></div>
-                    <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${data[13].repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
+                    <div className="cardLinks"><a href={`${data[12].link}`} target="_blank" rel="noreferrer">Live link</a></div>
                 </div>
             </div>
         </SwiperSlide>
 
-          {/* <SwiperSlide> */}
-            {/* <SwiperCard card={selectedCard} /> */}
         </Swiper>
     );
 };  
--- src/components/body/SwiperData.js
+++ src/components/body/SwiperData.js
@@ -5,7 +5,7 @@ export const data = [
     name: 'Puffy Cat Game ',
     description: 'Puffy Cat version of Flappy Bird coded with Javascript and HTML5 canvas and designed with custom graphics.',
     languages: 'Javascript, CSS, HTML5',
-    link: 'puffy-cat-game',
+    link: 'https://katarzyna-kw.github.io/puffy-cat-game',
     repo: 'puffy-cat-game'
   },
   {
@@ -14,7 +14,7 @@ export const data = [
     name: 'Github Repo Gallery',
     description: 'Displays public repos with search functionality. Javascript coded for Skillcrush challenge/learning purposes. HTML/CSS template customized to match portfolio website and to enable light/dark themes to enhance user experience.',
     languages: 'Javascript, CSS, HTML',
-    link: 'github-repo-gallery',
+    link: 'https://katarzyna-kw.github.io/github-repo-gallery',
     repo: 'github-repo-gallery'
   }, 
   {
@@ -23,7 +23,7 @@ export const data = [
     name: 'Sticky Notes Generator ',
     description: 'To-do app coded using React JS. HTML/CSS starter template customization.',
     languages: 'React JS, CSS',
-    link: 'sticky-notes',
+    link: 'https://katarzyna-kw.github.io/sticky-notes',
     repo: 'sticky-notes'
   }, 
   {
@@ -32,43 +32,43 @@ export const data = [
     name: 'Daily Mood Check ',
     description: 'A functional mood tracker built with React JS that uses Google Forms as an API.',
     languages: 'React JS, CSS',
-    link: 'daily-check',
+    link: 'https://katarzyna-kw.github.io/daily-check',
     repo: 'daily-check'
   }, 
-  {
-    id: '05',
-    imageid: '05.png',
-    name: 'Youtube Clone ',
-    description: 'Created for learning purposes using React JS and Node JS Express/PostgreSQL for server. Work in Progress. Currently renders videos from database and increments likes/dislikes for each video.',
-    languages: 'React JS, Javascript through Node JS, CSS',
-    link: 'youtube-clone',
-    repo: 'youtube-clone'
-  }, 
+  // {
+  //   id: '05',
+  //   imageid: '05.png',
+  //   name: 'Youtube Clone ',
+  //   description: 'Created for learning purposes using React JS and Node JS Express/PostgreSQL for server. Work in Progress. Currently renders videos from database and increments likes/dislikes for each video.',
+  //   languages: 'React JS, Javascript through Node JS, CSS',
+  //   link: 'youtube-clone',
+  //   repo: 'youtube-clone'
+  // }, 
   {
     id: '06',
     imageid: '06.png',
     name: 'Guess the Word Game ',
     description: 'A guessing game in which the player has 8 attempt to guess the correct word. Javascript coded for Skillcrush challenge/learning purposes using provided HTML/CSS.',
     languages: 'Javascript, HTML, CSS',
-    link: 'guess-the-word-game',
+    link: 'https://katarzyna-kw.github.io/guess-the-word-game',
     repo: 'guess-the-word-game'
   },
-  {
-    id: '07',
-    imageid: '07.png',
-    name: 'Lodash Clone',
-    description: 'An academic exercise writing code to mimic lodash library methods such as first, last, forEach, filter, map, and reduce.',
-    languages: 'Javascript',
-    link: 'lodash-clone',
-    repo: 'lodash-clone'
-  },
+  // {
+  //   id: '07',
+  //   imageid: '07.png',
+  //   name: 'Lodash Clone',
+  //   description: 'An academic exercise writing code to mimic lodash library methods such as first, last, forEach, filter, map, and reduce.',
+  //   languages: 'Javascript',
+  //   link: 'lodash-clone',
+  //   repo: 'lodash-clone'
+  // },
   {
     id: '08',
     imageid: '08.png',
     name: 'Election Map',
     description: 'An interactive 2020 U.S. presidential election map built using vanilla Javascript for a Skillcrush project. HTML/CSS starter template customization.',
     languages: 'Javascript, CSS, HTML',
-    link: 'election-map-project',
+    link: 'https://katarzyna-kw.github.io/election-map-project',
     repo: 'election-map-project'
   },
   {
@@ -77,7 +77,7 @@ export const data = [
     name: 'Cat Clock',
     description: 'A responsive clock with dark and light modes built with vanilla Javascript for a Skillcrush project. HTML/CSS starter template customization.',
     languages: 'Javascript, HTML, CSS',
-    link: 'lolcat',
+    link: 'https://katarzyna-kw.github.io/lolcat',
     repo: 'lolcat'
   },
   {
@@ -86,7 +86,7 @@ export const data = [
     name: 'Quote Generator',
     description: 'A responsive quote generator made using vanilla JS, HTML, and CSS for learning purposes.',
     languages: 'Javascript, HTML, CSS',
-    link: 'quote-generator',
+    link: 'https://katarzyna-kw.github.io/quote-generator',
     repo: 'quote-generator'
   },
   {
@@ -95,7 +95,7 @@ export const data = [
     name: 'CSS Technical Documentation',
     description: 'A responsive mock technical documentation page about CSS built for a freecodecamp project using HTML and CSS.',
     languages: 'HTML, CSS',
-    link: 'fcc-challenge-4',
+    link: 'https://katarzyna-kw.github.io/fcc-challenge-4',
     repo: 'fcc-challenge-4'
   },
   {
@@ -104,7 +104,7 @@ export const data = [
     name: 'Mock School Site',
     description: 'A responsive mock website for a Chicago-based Polish language school built for a freecodecamp project using HTML and CSS.',
     languages: 'HTML, CSS',
-    link: 'fcc-challenge-3',
+    link: 'https://katarzyna-kw.github.io/fcc-challenge-3',
     repo: 'fcc-challenge-3'
   },
   {
@@ -113,7 +113,7 @@ export const data = [
     name: '606 Academy',
     description: 'A responsive website for a Chicago-based youth basketball company built with React, HTML, and CSS.',
     languages: 'React JS, HTML, CSS',
-    link: '606-academy',
+    link: 'https://606basketballacademy.com/',
     repo: '606-academy'
   },
   {
@@ -122,7 +122,7 @@ export const data = [
     name: 'Unplugged',
     description: 'A responsive website coded with CSS and HTML using an Abobe Photoshop prototype for a Skillcrush project.',
     languages: 'HTML, CSS',
-    link: 'unplugged',
+    link: 'https://katarzyna-kw.github.io/unplugged/',
     repo: 'unplugged'
   },
   {
@@ -131,7 +131,7 @@ export const data = [
     name: 'Rogue Pickings',
     description: 'A responsive website coded with CSS and HTML using an Abobe Photoshop prototype for a Skillcrush project.',
     languages: 'HTML, CSS',
-    link: 'rogue-pickings',
+    link: 'https://katarzyna-kw.github.io/rogue-pickings/',
     repo: 'rogue-pickings'
   }
 ];
