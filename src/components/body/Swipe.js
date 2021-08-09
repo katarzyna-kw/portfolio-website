18f3803f6a1473b601a251b431653dff4c933a95
katarzyna_w <katarzyna.koch@gmail.com>
2021-08-08 21:22:32 -0500

    rm repos link

--- src/components/body/Swipe.js
+++ src/components/body/Swipe.js
@@ -37,7 +37,7 @@ function Swipe() {
                         <h3>{project.name} <span>/// Built using {project.languages}</span></h3>
                         <p>{project.description}</p>
                         <div className="cardLinks"><a href={`${project.link}`} target="_blank" rel="noreferrer">Live link</a></div>
-                        <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${project.repo}`} target="_blank" rel="noreferrer">Github repo</a></div>
+                        {/* <div className="cardLinks"><a href={`https://github.com/katarzyna-kw/${project.repo}`} target="_blank" rel="noreferrer">Github repo</a></div> */}
                     </div>
                 </div>
             </SwiperSlide>
