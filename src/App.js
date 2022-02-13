ff3f878ae9eebcf5fbe9782eecc5e3381d955b7c
katarzyna_w <katarzyna.koch@gmail.com>
2022-02-13 10:16:59 -0600

    formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { useState } from 'react'
-import Header from './components/header/header.component.jsx';
-import Body from './components/body/body.component.jsx';
-import Footer from './components/footer/footer.component.jsx';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/header/header.component.jsx";
+import Body from "./components/body/body.component.jsx";
+import Footer from "./components/footer/footer.component.jsx";
+import "./App.css";
 
 function App() {
-
-  const [theme, setTheme] = useState('pastel');
+  const [theme, setTheme] = useState("pastel");
 
   return (
-    <div className={`App ${theme}`}
-    data-testid="App" 
-    style={
-      {
-      background: `
+    <main
+      className={`App ${theme}`}
+      data-testid="App"
+      style={{
+        background: `
         linear-gradient(
           95deg, 
           var(--theme-page-background-left) 60%,
           var(--theme-page-background-right) 60%
-        )` 
-      }
-    }
-  >
-      <Header theme={theme} setTheme={setTheme}/>
+        )`,
+      }}
+    >
+      <Header theme={theme} setTheme={setTheme} />
       <Body />
       <Footer />
-    </div>
+    </main>
   );
 }
 
