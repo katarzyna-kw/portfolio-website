601fc83766e4fed6f0344aa701514667aa4cc64e
katarzyna_w <katarzyna.koch@gmail.com>
2022-02-14 15:59:32 -0600

    refactor skills

--- src/components/body/Skills.component/Skills.jsx
+++ src/components/body/Skills.component/Skills.jsx
@@ -1,3 +1,4 @@
+import skillsData from './skillsData'
 import '../styles.css'
 import '../../../App.css'
 
@@ -5,29 +6,18 @@ function Skills() {
   return (
         <div id="skills">
             <h2>Skills</h2>
-            <div className="skills-content">
-                <div className="skills-content-col1">
-                    <h5>Technical</h5>
-                    <ul>
-                        <li><span>JavaScript</span></li>
-                        <li><span>React JS</span></li>
-                        <li><span>Python</span></li>
-                        <li><span>HTML</span></li>
-                        <li><span>CSS</span></li>
-                        <li><span>Git</span></li>
-                        <li><span>PHP</span></li>
-                        <li><span>Figma & Photoshop</span></li>
-                        <li><span>PostgreSQL</span></li>
-                        <li><span>Jasmine & Jest testing</span></li>
+            <div className="skills__content">
+                  {skillsData.map((item) => (
+                    <div className="skills__content--col">
+                    <h5 className="skills__content--h5">{item.scope}</h5>
+                    <ul clasName="skills__content--ul">
+                      {item.skills.map((skill) => (
+                        <li className="skills__content--li"><span>{skill}</span></li>
+                      ))}
                     </ul>
-                </div>
-                <div className="skills-content-col3">
-                    <h5>Fluent Languages</h5>
-                    <ul>
-                        <li>English</li>
-                        <li>Polish</li>
-                    </ul>
-                </div>
+                  </div>
+                  )
+                  )}
             </div>
           </div>
   );
---
+++ src/components/body/Skills.component/skillsData.js
@@ -0,0 +1,26 @@
+const skillsData = [
+  {
+    "scope": "Technical",
+    "skills": [
+      "JavaScript",
+      "React JS",
+      "Python",
+      "HTML",
+      "CSS",
+      "Git",
+      "PHP",
+      "Figma & Photoshop",
+      "PostgreSQL",
+      "Jasmine & Jest testing"
+    ]
+  },
+  {
+    "scope": "Fluent Languages",
+    "skills": [
+      "English",
+      "Polish"
+    ]
+  }
+]
+
+export default skillsData
