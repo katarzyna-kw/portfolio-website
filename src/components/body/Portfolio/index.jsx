23ba700f69ea5f10f7bb0aa36967331a8e5e36dd
katarzyna_w <katarzyna.koch@gmail.com>
2022-02-23 18:14:30 -0600

    Portfolio styles

--- src/components/body/Portfolio/index.jsx
+++ src/components/body/Portfolio/index.jsx
@@ -1,15 +1,15 @@
-import '../styles.css';
-import Swipe from '../Swipe/index'
+import Swipe from "../Swipe/index";
+import "./styles.css"
 
 function Portfolio() {
   return (
     <div id="portfolio">
-      <h2>Portfolio</h2>
+      <h2 className="body__header">Portfolio</h2>
       <div className="portfolio-container">
         <Swipe />
       </div>
     </div>
-);
+  );
 }
 
 export default Portfolio;
