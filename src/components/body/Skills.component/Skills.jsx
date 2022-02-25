d8a3928e5d5f37562c8bd099ea811acca6dfdb5c
katarzyna_w <katarzyna.koch@gmail.com>
2022-02-24 18:50:16 -0600

    Skills component styles

--- src/components/body/Skills.component/Skills.jsx
+++ src/components/body/Skills.component/Skills.jsx
@@ -1,25 +1,25 @@
-import skillsData from './skillsData'
-import '../styles.css'
-import '../../../App.css'
+import skillsData from "./skillsData";
+import "./styles.css";
 
 function Skills() {
   return (
-        <div id="skills">
-            <h2>Skills</h2>
-            <div className="skills__content">
-                  {skillsData.map((item) => (
-                    <div className="skills__content--col">
-                    <h5 className="skills__content--h5">{item.scope}</h5>
-                    <ul clasName="skills__content--ul">
-                      {item.skills.map((skill) => (
-                        <li className="skills__content--li"><span>{skill}</span></li>
-                      ))}
-                    </ul>
-                  </div>
-                  )
-                  )}
-            </div>
+    <div id="skills">
+      <h2 className="body__header">Skills</h2>
+      <div className="skills__content">
+        {skillsData.map((item) => (
+          <div className="skills__content--col">
+            <h5 className="skills__content--h5">{item.scope}</h5>
+            <ul clasName="skills__content--ul">
+              {item.skills.map((skill) => (
+                <li className="skills__content--li">
+                  <span>{skill}</span>
+                </li>
+              ))}
+            </ul>
           </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
