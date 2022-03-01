9a9dcde78d3f45d8b7cbb29c47f4366d5370f0f0
katarzyna_w <katarzyna.koch@gmail.com>
2022-03-01 08:16:34 -0600

    edit About

--- src/components/body/About.component/AboutText.jsx
+++ src/components/body/About.component/AboutText.jsx
@@ -5,7 +5,7 @@ function AboutText() {
     <div>
       <p className="about__content--text">
         My name is Katarzyna Wegrzynowicz. I am a highly-organized and
-        detail-oriented full-stack developer based in Round Lake Beach, IL. I
+        detail-oriented full-stack developer based in the greater Chicago area, Illinois. I
         love deconstructing problems to develop solutions and have a strong
         background in education. I get excited about tackling new challenges,
         lifelong learning, and making things beautiful.
@@ -15,7 +15,7 @@ function AboutText() {
         <a className="about__content--link" href="#skills">
           programming skills
         </a>{" "}
-        include Javascript, React, Python, PHP, HTML/CSS, and PostgreSQL. I love
+        include Javascript, React, Python, Django, HTML/CSS, and PostgreSQL. I love
         colors, animals, and Dionne Warwick.
       </p>
     </div>
